05c2e8c0d553e516304f555b46eb26d355a06b24
Ediz Agarer <ediz.agarer@teamdefinex.com>
2023-08-18 18:19:42 +0300

    Added configs

--- server.js
+++ server.js
@@ -1,12 +1,19 @@
 const express = require('express');
+
+// Environment variables
+const dotenv = require('dotenv');
+dotenv.config({
+  path: './config/env/config.env'
+})
+
 const app = express();
-const PORT = 5000 || process.env.PORT;
+const PORT = process.env.PORT || 5000;
 
 app.get("/", (req,res) => {
   res.send("Welcome Question Answer API");
 })
 
 app.listen(PORT, () => {
-  console.log(`App Started on ${PORT}`);
+  console.log(`App Started on ${PORT} : ${process.env.NODE_ENV}`);
 })
 
