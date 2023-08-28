12085ec252db875d16fbd99b43a182fcdc004988
Ediz Agarer <edizagarer@gmail.com>
2023-08-29 00:13:43 +0300

    Added Async Error Handler

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,35 +1,29 @@
 const User = require('../models/User');
 const CustomError = require('../helpers/error/CustomError');
+const asyncErrorWrapper = require('express-async-handler');
 
-const register = async (req, res, next) => {
+const register = asyncErrorWrapper(async (req, res, next) => {
   const name = "Ediz Ağarer"
   const email = "edizagarer@gmail.com"
   const password = "1234"
 
+  const user = await User.create({
+    name,
+    email,
+    password
+  })
 
-  try {
-    const user = await User.create({
-      name,
-      email,
-      password
-    })
-
-    res.status(200).json({
-      success: true,
-      data: user
-    })
-  } catch (err) {
-    return next(err)
-  }
-
-
-}
+  res.status(200).json({
+    success: true,
+    data: user
+  })
+});
 
 const errorTest = (req, res, next) => {
   // throw new Error("Bir hata oluştu")
 
   // return next(new Error("Bir hata oluştu"))
-  
+
   // return next(new CustomError("Custom Error Message", 400))
 
   return next(new TypeError("Type Error"))
