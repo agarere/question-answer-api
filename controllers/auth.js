2c9adb65fe59f83e6e13c084f6e4dd4d909b724a
Ediz Agarer <edizagarer@gmail.com>
2023-08-28 23:59:37 +0300

    Added Error Handling

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,22 +1,41 @@
 const User = require('../models/User');
+const CustomError = require('../helpers/error/CustomError');
 
 const register = async (req, res, next) => {
   const name = "Ediz Ağarer"
   const email = "edizagarer@gmail.com"
-  const password = "123456"
-  // todo:
-  const user = await User.create({
-    name,
-    email,
-    password
-  })
+  const password = "1234"
 
-  res.status(200).json({
-    success: true,
-    data: user
-  })
+
+  try {
+    const user = await User.create({
+      name,
+      email,
+      password
+    })
+
+    res.status(200).json({
+      success: true,
+      data: user
+    })
+  } catch (err) {
+    return next(err)
+  }
+
+
+}
+
+const errorTest = (req, res, next) => {
+  // throw new Error("Bir hata oluştu")
+
+  // return next(new Error("Bir hata oluştu"))
+  
+  // return next(new CustomError("Custom Error Message", 400))
+
+  return next(new TypeError("Type Error"))
 }
 
 module.exports = {
-  register
+  register,
+  errorTest
 }
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const routers = require('./routers');
 const connectDatabase = require('./helpers/database/ConnectDatabase');
+const customErrorHandler = require('./middlewares/errors/CustomErrorHandler')
 
 // Environment variables
 const dotenv = require('dotenv');
@@ -15,6 +16,8 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use("/api", routers);
+
+app.use(customErrorHandler)
 
 app.listen(PORT, () => {
   console.log(`App Started on ${PORT} : ${process.env.NODE_ENV}`);
