2d45c227709c974986e5a5e5d88e8a67291917f4
Ediz Agarer <edizagarer@gmail.com>
2023-09-22 15:56:43 +0300

    Added logout

--- routers/auth.js
+++ routers/auth.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { register, errorTest, getUser, login } = require('../controllers/auth')
+const { register, errorTest, getUser, login, logout } = require('../controllers/auth')
 
 const { getAccessToRoute } = require('../middlewares/authorization/auth')
 
@@ -10,5 +10,6 @@ router.post("/register", register)
 router.get("/error", errorTest)
 router.get("/profile", getAccessToRoute, getUser)
 router.post("/login", login)
+router.post("/logout", getAccessToRoute, logout)
 
 module.exports = router;
